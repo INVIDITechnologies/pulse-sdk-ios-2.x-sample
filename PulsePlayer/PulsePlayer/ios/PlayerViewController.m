93ffa6104cec7aa7f71b85af8b6f298bc97f68d3
Priti Verma <pverma@ooyala.com>
2020-03-19 07:57:18 +0100

    updated the way OmidAdSession is initialized

--- PulsePlayer/PulsePlayer/ios/PlayerViewController.m
+++ PulsePlayer/PulsePlayer/ios/PlayerViewController.m
@@ -316,8 +316,8 @@ typedef enum : NSUInteger {
 
   [self.adAsset preloadWithTimeout:timeout success:^(AVAsset *asset) {
     self.videoAd = ad;
-      OmidAdSession * omidAdSession = [OmidAdSession new];
-      [omidAdSession createOmidAdSession:self.parentViewController];
+      OmidAdSession * omidAdSession = [OmidAdSession getInstance];
+      [omidAdSession createOmidAdSession:self.view];
     [self play:[AVPlayerItem playerItemWithAsset:asset]];
   } failure:^(OOPulseAdError error) {
     self.adAsset = nil;
