13fd0ba1c86184c7b8f77bed14fb49d5b5a8802e
Priti Verma <pverma@invidi.com>
2021-03-22 16:36:32 +0100

    removed unwanted comment

--- PulsePlayer/PulsePlayer/ios/NextAdThumbnailController.m
+++ PulsePlayer/PulsePlayer/ios/NextAdThumbnailController.m
@@ -16,13 +16,6 @@
 
 @implementation NextAdThumbnailController
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.hidden = YES;
